31027442c4961ee3de238404c7fde60c598c68d2
Connor Sheedy <connor@ensodata.com>
2025-10-24 10:47:14 -0500

    I added better python support for linting and formatting with ruff and black using conform

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -1,76 +1,89 @@
-vim.lsp.enable({ "lua_ls", "pylsp", "luasnip", "codecompanion"})
+local check_for_poetry_lock = function()
+	local match = vim.fn.glob(vim.fn.getcwd() .. "/poetry.lock")
+	if vim.env.VIRTUAL_ENV ~= nil then
+		return nil
+	end
+	if match ~= "" then
+		local poetry_venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+		local path_extended = string.format("%s:%s/bin", vim.env.PATH, poetry_venv)
+		vim.env.VIRTUAL_ENV = poetry_venv
+		vim.env.PATH = path_extended
+	end
+end
 
-local capabilities = require('blink.cmp').get_lsp_capabilities()
+-- Run this before we enable our LSP
+check_for_poetry_lock()
+local capabilities = require("blink.cmp").get_lsp_capabilities()
 vim.lsp.config("*", { capabilities = capabilities })
+vim.lsp.enable({ "lua_ls", "pylsp", "luasnip" })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('my.lsp', {}),
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, {
-        autotrigger = true,
-        convert = function(item)
-          return { abbr = item.label:gsub('%b()', '') }
-        end,
-      })
-      local builtin = require "telescope.builtin"
-      vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
-      vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
-      vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
-      vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
-      vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
-      vim.keymap.set("n", '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("my.lsp", {}),
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client:supports_method("textDocument/completion") then
+			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+			vim.lsp.completion.enable(true, client.id, ev.buf, {
+				autotrigger = true,
+				convert = function(item)
+					return { abbr = item.label:gsub("%b()", "") }
+				end,
+			})
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "gd", builtin.lsp_definitions, { buffer = 0 })
+			vim.keymap.set("n", "gr", builtin.lsp_references, { buffer = 0 })
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = 0 })
+			vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, { buffer = 0 })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = 0 })
+			vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, { buffer = 0 })
+			vim.keymap.set("n", "<space>wd", builtin.lsp_document_symbols, { buffer = 0 })
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = 0 })
 
-      vim.keymap.set('i', "<C-Space>", function()
-        vim.lsp.completion.get()
-      end)
-    end
-    -- if not client:supports_method('textDocument/willSaveWaitUntil')
-    --     and client:supports_method('textDocument/formatting') then
-    --   vim.api.nvim_create_autocmd('BufWritePre', {
-    --     group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-    --     buffer = ev.buf,
-    --     callback = function()
-    --       vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-    --     end,
-    --   })
-    -- end
-  end,
+			vim.keymap.set("i", "<C-Space>", function()
+				vim.lsp.completion.get()
+			end)
+		end
+		-- if not client:supports_method('textDocument/willSaveWaitUntil')
+		--     and client:supports_method('textDocument/formatting') then
+		--   vim.api.nvim_create_autocmd('BufWritePre', {
+		--     group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
+		--     buffer = ev.buf,
+		--     callback = function()
+		--       vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+		--     end,
+		--   })
+		-- end
+	end,
 })
 
 vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true
-  }
+	virtual_lines = {
+		current_line = true,
+	},
 })
 
-local cmp = require "cmp"
-cmp.setup {
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)      -- For `luasnip` users.
-      require('snippy').expand_snippet(args.body)   -- For `snippy` users.
-      vim.snippet.expand(args.body)                 -- For native neovim snippets (Neovim v0.10+)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' }, -- For luasnip users.
-    { name = 'blink' }, -- For luasnip users.
-    -- { name = 'snippy' },
-    { name = "codecompanion"},
-    { name = 'buffer' },
-  })
-}
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			require("snippy").expand_snippet(args.body) -- For `snippy` users.
+			vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+		{ name = "blink" }, -- For luasnip users.
+		-- { name = 'snippy' },
+		{ name = "buffer" },
+	}),
+})
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,268 +1,292 @@
-require('lazy').setup({
-    -- NOTE: First, some plugins that don't require any configuration
-    -- Git related plugins
-    'tpope/vim-fugitive',
-    -- File/project manager
-    'ThePrimeagen/harpoon',
-    -- Detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
-    --Tmux supports
-    "aserowy/tmux.nvim",
-    "mbbill/undotree",
-    'echasnovski/mini.nvim',
-    -- lazy.nvim
-    {
-      "olimorris/codecompanion.nvim",
-      dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-treesitter/nvim-treesitter",
-      },
-      opts = {
-        -- NOTE: The log_level is in `opts.opts`
-        opts = {
-          log_level = "DEBUG", -- or "TRACE"
-        },
-      },
-    },
-    { 'nvim-mini/mini.surround', version = false },
-    {
-      'stevearc/oil.nvim',
-      opts = {},
-      -- Optional dependencies
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        require("oil").setup({
-          delete_to_trash = true,
-          skip_confirm_for_simple_edits = true,
-          view_options = {
-            show_hidden = true,
-            natural_order = true,
-            is_always_hidden = function(name, _)
-              return name == '..' or name == '.git'
-            end
-          }
-        })
-      end,
-    },
-    -- NOTE: This is where your plugins related to LSP can be installed.
-    --  The configuration is done below. Search for lspconfig to find it below.
-    {
-      "neovim/nvim-lspconfig",
-      dependencies = {
-        -- Automatically install LSPs to stdpath for neovim
-        { 'williamboman/mason.nvim', config = true },
-        'williamboman/mason-lspconfig.nvim',
+require("lazy").setup({
+	-- NOTE: First, some plugins that don't require any configuration
+	-- Git related plugins
+	"tpope/vim-fugitive",
+	-- File/project manager
+	"ThePrimeagen/harpoon",
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
+	--Tmux supports
+	"aserowy/tmux.nvim",
+	"mbbill/undotree",
+	"echasnovski/mini.nvim",
+	-- lazy.nvim
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		opts = {
+			-- NOTE: The log_level is in `opts.opts`
+			opts = {
+				log_level = "DEBUG", -- or "TRACE"
+			},
+		},
+	},
+	{ "nvim-mini/mini.surround", version = false },
+	{
+		"stevearc/oil.nvim",
+		opts = {},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				delete_to_trash = true,
+				skip_confirm_for_simple_edits = true,
+				view_options = {
+					show_hidden = true,
+					natural_order = true,
+					is_always_hidden = function(name, _)
+						return name == ".." or name == ".git"
+					end,
+				},
+			})
+		end,
+	},
+	-- NOTE: This is where your plugins related to LSP can be installed.
+	--  The configuration is done below. Search for lspconfig to find it below.
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- Automatically install LSPs to stdpath for neovim
+			{ "williamboman/mason.nvim", config = true },
+			"williamboman/mason-lspconfig.nvim",
 
-        -- Useful status updates for LSP
-        -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-        { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
+			-- Useful status updates for LSP
+			-- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+			{ "j-hui/fidget.nvim", tag = "legacy", opts = {} },
 
-        -- Additional lua configuration, makes nvim stuff amazing!
-        'folke/neodev.nvim',
-      },
-    },
-    {
-      -- Autocompletion
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
+			-- Additional lua configuration, makes nvim stuff amazing!
+			"folke/neodev.nvim",
+		},
+	},
+	{
+		-- Autocompletion
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
 
-        -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
+			-- Adds LSP completion capabilities
+			"hrsh7th/cmp-nvim-lsp",
 
-        -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
-      },
-    },
-    -- Useful plugin to show you pending keybinds.
-    {
-      -- Adds git related signs to the gutter, as well as utilities for managing changes
-      'lewis6991/gitsigns.nvim',
-      opts = {
-        -- See `:help gitsigns.txt`
-        signs = {
-          add = { text = '+' },
-          change = { text = '~' },
-          delete = { text = '_' },
-          topdelete = { text = '‾' },
-          changedelete = { text = '~' },
-        },
-        on_attach = function(bufnr)
-          vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-            { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-          vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk,
-            { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
-          vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk,
-            { buffer = bufnr, desc = '[P]review [H]unk' })
-          vim.keymap.set('n', '<leader>rh', require('gitsigns').reset_hunk, { buffer = bufnr, desc = '[R]eset [H]unk' })
-        end,
-      },
-    },
-    {
-      -- Set lualine as statusline
-      'nvim-lualine/lualine.nvim',
-      -- See `:help lualine.txt`
-      opts = {
-        options = {
-          icons_enabled = false,
-          theme = 'rose-pine',
-          component_separators = '|',
-          section_separators = '',
-        },
-      },
-    },
-    { "rose-pine/neovim",                    name = "rose-pine" },
-    { "lukas-reineke/indent-blankline.nvim", main = "ibl",      opts = {} },
-    -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim',               opts = {} },
+			-- Adds a number of user-friendly snippets
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	-- Useful plugin to show you pending keybinds.
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set(
+					"n",
+					"<leader>gp",
+					require("gitsigns").prev_hunk,
+					{ buffer = bufnr, desc = "[G]o to [P]revious Hunk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>gn",
+					require("gitsigns").next_hunk,
+					{ buffer = bufnr, desc = "[G]o to [N]ext Hunk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>ph",
+					require("gitsigns").preview_hunk,
+					{ buffer = bufnr, desc = "[P]review [H]unk" }
+				)
+				vim.keymap.set(
+					"n",
+					"<leader>rh",
+					require("gitsigns").reset_hunk,
+					{ buffer = bufnr, desc = "[R]eset [H]unk" }
+				)
+			end,
+		},
+	},
+	{
+		-- Set lualine as statusline
+		"nvim-lualine/lualine.nvim",
+		-- See `:help lualine.txt`
+		opts = {
+			options = {
+				icons_enabled = false,
+				theme = "rose-pine",
+				component_separators = "|",
+				section_separators = "",
+			},
+		},
+	},
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
 
-    -- Fuzzy Finder (files, lsp, etc)
-    {
-      'nvim-telescope/telescope.nvim',
-      branch = '0.1.x',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
-        {
-          'nvim-telescope/telescope-fzf-native.nvim',
-          -- NOTE: If you are having trouble with this installation,
-          --       refer to the README for telescope-fzf-native for more instructions.
-          build = 'make',
-          cond = function()
-            return vim.fn.executable 'make' == 1
-          end,
-        },
-      },
-    },
-    {
-      'nvim-treesitter/nvim-treesitter', opts = {}
-    },
-    {
-      "epwalsh/obsidian.nvim",
-      version = "*", -- recommended, use latest release instead of latest commit
-      lazy = true,
-      ft = "markdown",
-      -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-      -- event = {
-      --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-      --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-      --   "BufReadPre path/to/my-vault/**.md",
-      --   "BufNewFile path/to/my-vault/**.md",
-      -- },
-      dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
+	-- Fuzzy Finder (files, lsp, etc)
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			-- Fuzzy Finder Algorithm which requires local dependencies to be built.
+			-- Only load if `make` is available. Make sure you have the system
+			-- requirements installed.
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				-- NOTE: If you are having trouble with this installation,
+				--       refer to the README for telescope-fzf-native for more instructions.
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {},
+	},
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		--   "BufReadPre path/to/my-vault/**.md",
+		--   "BufNewFile path/to/my-vault/**.md",
+		-- },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
 
-        -- see below for full list of optional dependencies 👇
-      },
-      opts = {
-        workspaces = {
-          {
-            name = "work",
-            path = "~/ensodata/projects",
-          }
-        },
-        -- see below for full list of options 👇
-      },
-      mappings = {
-        -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        ["gf"] = {
-          action = function()
-            return require("obsidian").util.gf_passthrough()
-          end,
-          opts = { noremap = false, expr = true, buffer = true },
-        },
-        -- Toggle check-boxes.
-        ["<leader>ch"] = {
-          action = function()
-            return require("obsidian").util.toggle_checkbox()
-          end,
-          opts = { buffer = true },
-        },
-        -- Smart action depending on context, either follow link or toggle checkbox.
-        ["<cr>"] = {
-          action = function()
-            return require("obsidian").util.smart_action()
-          end,
-          opts = { buffer = true, expr = true },
-        }
-      },
-      ui = {
-        enable = false,
-      },
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "work",
+					path = "~/ensodata/projects",
+				},
+			},
+			-- see below for full list of options 👇
+		},
+		mappings = {
+			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+			["gf"] = {
+				action = function()
+					return require("obsidian").util.gf_passthrough()
+				end,
+				opts = { noremap = false, expr = true, buffer = true },
+			},
+			-- Toggle check-boxes.
+			["<leader>ch"] = {
+				action = function()
+					return require("obsidian").util.toggle_checkbox()
+				end,
+				opts = { buffer = true },
+			},
+			-- Smart action depending on context, either follow link or toggle checkbox.
+			["<cr>"] = {
+				action = function()
+					return require("obsidian").util.smart_action()
+				end,
+				opts = { buffer = true, expr = true },
+			},
+		},
+		ui = {
+			enable = false,
+		},
 
-      -- Specify how to handle attachments.
-      attachments = {
-        -- The default folder to place images in via `:ObsidianPasteImg`.
-        -- If this is a relative path it will be interpreted as relative to the vault root.
-        -- You can always override this per image by passing a full path to the command instead of just a filename.
-        img_folder = "assets/imgs", -- This is the default
-        -- A function that determines the text to insert in the note when pasting an image.
-        -- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
-        -- This is the default implementation.
-        ---@param client obsidian.Client
-        ---@param path obsidian.Path the absolute path to the image file
-        ---@return string
-        img_text_func = function(client, path)
-          path = client:vault_relative_path(path) or path
-          return string.format("![%s](%s)", path.name, path)
-        end,
-      },
-      spec = {
-        { import = "lazyvim.plugins.extras.lang.python" },
-      },
-    },
-    {
-      'saghen/blink.cmp',
-      -- optional: provides snippets for the snippet source
-      dependencies = { 'rafamadriz/friendly-snippets' },
+		-- Specify how to handle attachments.
+		attachments = {
+			-- The default folder to place images in via `:ObsidianPasteImg`.
+			-- If this is a relative path it will be interpreted as relative to the vault root.
+			-- You can always override this per image by passing a full path to the command instead of just a filename.
+			img_folder = "assets/imgs", -- This is the default
+			-- A function that determines the text to insert in the note when pasting an image.
+			-- It takes two arguments, the `obsidian.Client` and an `obsidian.Path` to the image file.
+			-- This is the default implementation.
+			---@param client obsidian.Client
+			---@param path obsidian.Path the absolute path to the image file
+			---@return string
+			img_text_func = function(client, path)
+				path = client:vault_relative_path(path) or path
+				return string.format("![%s](%s)", path.name, path)
+			end,
+		},
+		spec = {
+			{ import = "lazyvim.plugins.extras.lang.python" },
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = { "rafamadriz/friendly-snippets" },
 
-      -- use a release tag to download pre-built binaries
-      version = '1.*',
-      -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-      -- build = 'cargo build --release',
-      -- If you use nix, you can build from source using latest nightly rust with:
-      -- build = 'nix run .#build-plugin',
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
 
-      ---@module 'blink.cmp'
-      ---@type blink.cmp.Config
-      opts = {
-        -- See :h blink-cmp-config-keymap for defining your own keymap
-        keymap = { preset = 'default' },
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = { preset = "default" },
 
-        appearance = {
-          -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-          -- Adjusts spacing to ensure icons are aligned
-          nerd_font_variant = 'mono'
-        },
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
 
-        -- (Default) Only show the documentation popup when manually triggered
-        completion = { documentation = { auto_show = false } },
+			-- (Default) Only show the documentation popup when manually triggered
+			completion = { documentation = { auto_show = false } },
 
-        -- Default list of enabled providers defined so that you can extend it
-        -- elsewhere in your config, without redefining it, due to `opts_extend`
-        sources = {
-          default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+				per_filetype = {
+					codecompanion = { "codecompanion" },
+				},
+			},
 
-        -- See the fuzzy documentation for more information
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-      },
-      opts_extend = { "sources.default" }
-    },
-    {
-      "mason-org/mason-lspconfig.nvim",
-      opts = {
-        ensure_installed = { "lua_ls", "pylsp", "ruff" }
-      },
-      dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-      },
-    },
-  },
-  {})
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+		},
+		opts_extend = { "sources.default" },
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = { "lua_ls", "pylsp", "ruff" },
+		},
+		dependencies = {
+			{ "mason-org/mason.nvim", opts = {} },
+			"neovim/nvim-lspconfig",
+		},
+	},
+}, {})
