e27de3745c9ee62a0fa2b024b9e94453aaa62c1f
Connor Sheedy <connor@ensodata.com>
2026-03-31 14:22:01 -0500

    Update to 0.12

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -28,8 +28,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1 }) end, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1 }) end, { desc = "Go to next diagnostic message" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Harpoon keymaps
